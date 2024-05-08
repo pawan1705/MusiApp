f8eaaa7f05f2abe712258f7fca5077d59c5f879f
pawan1705 <pawan.sangare05@gmailcom>
2024-05-08 20:08:51 +0530

    first

---
+++ src/Component/Sidebar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {assets } from '../assets/assets';
+const Sidebar  = () => {
+  return (
+    <div className="w-[25%] h-full p-4 flex-col gap-2 text-white hidden lg:flex">
+        <div className="bg-[#474747] h-[120px] rounded flex flex-col justify-around">
+            <div className="flex items-center gap-3 pl-8 cursor-pointer">
+                <img className='w-6' src={assets.home_icon} alt="no img" />
+                <p className='font-bold'>Home</p>
+            </div>
+            <div className="flex items-center gap-3 pl-8 cursor-pointer">
+                <img className='w-6' src={assets.search_icon} alt="no img" />
+                <p className='font-bold'>Search</p>
+            </div>
+        </div>
+        <div className="bg-[#474747] h-[350px] rounded " >
+            <div className="p-2 flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <img className='w-8' src={assets.stack_icon} alt="" />
+                    <p className='font-semibold'>Your Library</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <img className='w-5' src={assets.arrow_icon} alt="" />
+                    <img className='w-5' src={assets.plus_icon} alt="" />
+                </div>
+            </div>
+            <div className="p-2 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1  pl-4">
+                <h1>Create Your First Playlist</h1>
+                <p className='font-light'>It's easy we will help you.</p>
+                <button className='px-4 py-0.5 bg-white text-[15px] text-black rounded-full mt-2'>Create Playlist</button>
+            </div>
+            <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1  pl-4">
+                <h1>Let's Find Some Podcasts to follow.</h1>
+                <p className='font-light'>We will keep you update on new episode.</p>
+                <button className='px-4 py-0.5 bg-white text-[15px] text-black rounded-full mt-2 '>Browse podcast</button>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar;
